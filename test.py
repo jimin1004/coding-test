92f2a0916f667226d351d3b6037b6a03a3b15870
jimin1004 <130583366+jimin1004@users.noreply.github.com>
2023-10-22 20:08:08 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1476 ms, Memory: 76972 KB -BaekjoonHub(수동 업로드)

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 import sys
 
-sys.stdin = open("input_2751.txt", "r")
+# sys.stdin = open('input_2751.txt', 'r')
 n = int(sys.stdin.readline())
 nums = []  # 정렬할 숫자가 들어갈 리스트
 
 for i in range(n):  ## 모든 숫자 일단 리스트에 저장
     nums.append(int(sys.stdin.readline().rstrip()))
 
+
 nums.sort()
+print(nums)
 
 for i in nums:
     print(i)
