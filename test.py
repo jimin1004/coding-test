f939998147f2cf9a3e7b1bfcb3f616d55ec5ee6d
jimin1004 <130583366+jimin1004@users.noreply.github.com>
2023-10-26 22:47:47 +0900

    solve BOJ 2839

--- test.py
+++ test.py
@@ -1,15 +1,7 @@
 import sys
 
-# sys.stdin = open('input_2751.txt', 'r')
-n = int(sys.stdin.readline())
-nums = []  # 정렬할 숫자가 들어갈 리스트
+# N = int(sys.stdin.readline())
+N = int(input())
 
-for i in range(n):  ## 모든 숫자 일단 리스트에 저장
-    nums.append(int(sys.stdin.readline().rstrip()))
-
-
-nums.sort()
-print(nums)
-
-for i in nums:
+for i in range(10, 0, -1):
     print(i)
