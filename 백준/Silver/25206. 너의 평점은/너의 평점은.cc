c08094449a25d842066437fc1d9037a8f0a7605b
jimin1004 <130583366+jimin1004@users.noreply.github.com>
2023-11-26 00:38:03 +0900

    [Silver V] Title: 너의 평점은, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+
+using namespace std;
+
+char sub_name[51] = {0};
+double credit[20] = {0};
+double credit_sum = 0;
+double res = 0;
+pair<const char *, double> grade[10] = {{"A+", 4.5}, {"A0", 4.0}, {"B+", 3.5}, {"B0", 3.0}, {"C+", 2.5}, {"C0", 2.0}, {"D+", 1.5}, {"D0", 1.0}, {"F", 0.0}, {"P", 0.0}};
+char grade_s[3] = {0};
+int grade_p;
+
+int main()
+{
+    for (int i = 0; i < 20; i++)
+    {
+        cin >> sub_name >> credit[i] >> grade_s;
+        // cout << sub_name << credit[i] << grade_s << '\n';
+
+        // P 과목 제외
+        if (strcmp(grade_s, "P") == 0)
+        {
+            credit[i] = 0;
+            //  cout << "hit";
+        }
+
+        // 학점을 점수로 환산할 수 있게 학점 위치 가리키는 포인터에 저장
+        for (int j = 0; j < 10; j++)
+        {
+            if (strcmp(grade_s, grade[j].first) == 0)
+            {
+                grade_p = j;
+            }
+        }
+        res += credit[i] * grade[grade_p].second;
+        // cout << credit[i] << " " << grade[grade_p].second << '\n';
+    }
+
+    // 수강 학점 합
+    for (int i = 0; i < 20; i++)
+    {
+        credit_sum += credit[i];
+        // cout << credit[i] << '\n';
+    }
+
+    cout << fixed;
+    cout << res / credit_sum;
+
+    return 0;
+}
