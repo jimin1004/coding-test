f735bed5b3e97adde2c95a6f70826de2afcfe607
jimin1004 <130583366+jimin1004@users.noreply.github.com>
2023-12-25 18:16:19 +0900

    [level 2] Title: 주식가격, Time: 14.97 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -6,13 +6,6 @@ using namespace std;
 
 vector<int> solution(vector<int> prices) {
     vector<int> answer(prices.size());
-    vector<int> s(prices.size());
-    vector<int> d(prices.size()-1);
-    
-//     for(int i = ; i < d.size(); i++) {
-//         d[i] = prices[i+1] - prices[i];
-//         //cout << d[i] << "\n";
-// }
     
     for (int i = 0; i < prices.size()-1; i++) {
           for (int j = i+1; j < prices.size(); j++) {
